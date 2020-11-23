19e7c00e6ad9811b0384c73f370d185648b018d5
callummole <cmole@turing.ac.uk>
2020-11-23 16:04:50 +0000

    updated azure edits

---
+++ deployment/sandbox-v2/automated_install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# for automated deployment of mosip infrastructure on a mac
+#pre-requisits
+echo "installing pre-requisites, terraform and azure-cli"
+brew install terraform
+brew install azure-cli
+#hard-coded edits
+echo "correcting hardcoded edits"
+cd ~
+git clone https://github.com/alan-turing-institute/mosip-infra/
+cd mosip-infra
+git checkout simple-mac-deploy
+cd deployment/sandbox-v2
+chmod u+x ./turing_azure_edits.sh
+./turing_azure_edits.sh
+# azure terraform deployment
+echo "azure terraform deployment"
+az login
+cd terraform/azure
+terraform init
+
--- deployment/sandbox-v2/turing_azure_edits.sh
+++ deployment/sandbox-v2/turing_azure_edits.sh
@@ -1,21 +1,38 @@
 #!/bin/sh
 
 #this program edits all the hardcoded MOSIP variables to work with azure deployment.
-#stick this file in your mosip-infra root folder.
-#in terminal navigate to mosip-infra root folder and enter 'chmod u+x mosip_azure_edits.sh' so the user has permissions to run the shell script
-#enter ./mosip_azure_edits.sh
+#stick this file in your sandbox-v2 root folder.
+#in terminal navigate to mosip-infra root folder and enter 'chmod u+x turing_azure_edits.sh' so the user has permissions to run the shell script
+#enter ./turing_azure_edits.sh
 #note that the changes will also happen to this file. So if you make a mistake in the region you can just run the script again and enter the correct region
 
-#STEP 1 - change hardcoded regions
-read -p "Azure Region (Title Case): " region
+#STEP 1 change files in variables.tf
+#read -p "Azure Subscription ID: " subid
+subid="a8604fcd-7cc8-4801-a975-a4b777a179a4"
+sed -i "" "3s/834c8af0-effa-4ca8-8bb6-1c499c21a323/$subid/" ./terraform/azure/variables.tf
+echo "updated Azure subscription id"
+
+#read -p "Admin Password: " adm
+adm="roli7219@ati"
+sed -i "" "20s/Password@123/$adm/" ./terraform/azure/variables.tf
+echo "updated password"
+
+#read -p "Domain name label (Azure DNS name): " dns
+dns="configure.uksouth.cloudapp.azure.com"
+sed -i "" "12s/test-machine/$dns/" ./terraform/azure/variables.tf
+echo "updated domain name"
+ 
+#STEP 2 - change hardcoded regions
+#read -p "Azure Region (Title Case): " region
+region="UK South"
 regionlc=${region// /} #remove whitespace
 regionlc=$(echo "$regionlc" | tr A-Z a-z) #convert to lowercase
 LC_CTYPE=C find ./ -type f -exec sed -i "" "s/South India/$region/g" {} +
-LC_CTYPE=C find ./ -type f -exec sed -i "" "s/southindia/$regionlc/g" {} +
+LC_CTYPE=C find ./ -type f -exec sed -i "" "s/uksouth/$regionlc/g" {} +
 
 echo "Changed all hardcoded title case and lowercase region entries"
 
-#STEP 2 -delete  from everything, even the aws directory. NOTE will have to reset repo if want to deploy aws
-LC_CTYPE=C find ./ -type f -exec sed -i "" "s/.sb//g" {} +
+#STEP 3 -delete unwanted file extentions from everything. NOTE will have to reset repo if want to deploy aws.
+LC_CTYPE=C find ./ -type f -exec sed -i "" "s///g" {} +
 
 echo "deleted unwanted file extensions"
