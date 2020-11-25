2023e22ac81083a649ae3b2bb02d991ecaadceef
callummole <cmole@turing.ac.uk>
2020-11-25 18:47:33 +0000

    edit branch

--- deployment/sandbox-v2/automated_MOSIP_deployment.sh
+++ deployment/sandbox-v2/automated_MOSIP_deployment.sh
@@ -10,14 +10,19 @@ echo "STEP 2: CORRECTING HARDCODED EDITS"
 cd ~
 git clone https://github.com/alan-turing-institute/mosip-infra/
 cd mosip-infra
-git checkout simple-mac-deploy
+git checkout simple-mac-deploy-1.1.2
 cd deployment/sandbox-v2
 chmod u+x ./turing_azure_edits.sh
 ./turing_azure_edits.sh
 # azure terraform deployment
+chmod u+x ./install_MOSIP_vm.sh
+chmod u+x ./startup_vms.sh
+chmod u+x ./stop_vms.sh
+
 echo "STEP 3: AZURE TERRAFORM DEPLOYMENT"
 az login
 cd terraform/azure
 terraform init
 terraform plan
+echo "RUNNING TERRAFORM APPLY, EXPECT ERROR 404 AND RUN AGAIN"
 terraform apply
