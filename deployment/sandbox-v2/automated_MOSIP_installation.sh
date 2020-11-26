3f67ab8e03cb2dfb5179e5898fadccd7a707d965
callummole <cmole@turing.ac.uk>
2020-11-26 09:45:55 +0000

    edit echo message

--- deployment/sandbox-v2/automated_MOSIP_installation.sh
+++ deployment/sandbox-v2/automated_MOSIP_installation.sh
@@ -2,5 +2,5 @@
 
 # script to install MOSIP on vm
 ./edit_MOSIP_files.sh
-echo "RUNNING INSTALLATION ON MOSIP"
+echo "RUNNING MOSIP INSTALLATION ON CONSOLE"
 az vm run-command invoke -g mosip-sandbox-test -n console --command-id RunShellScript --scripts @install_MOSIP_vm.sh
