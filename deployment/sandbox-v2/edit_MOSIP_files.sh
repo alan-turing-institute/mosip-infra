ef0d301baef819e49ee18f2a043430cb32ba3b5c
callummole <cmole@turing.ac.uk>
2020-11-26 11:48:31 +0000

    regex existing IPs

--- deployment/sandbox-v2/edit_MOSIP_files.sh
+++ deployment/sandbox-v2/edit_MOSIP_files.sh
@@ -15,37 +15,37 @@ az account set -s a8604fcd-7cc8-4801-a975-a4b777a179a4
 vm="console"
 ip=$(az vm show -g mosip-sandbox-test -n $vm --query privateIps -d --out tsv)
 sed -i "" "4s/$vm.sb/$vm/" ./hosts.ini
-sed -i "" "4s/[0-9]*[[punct]][0-9]*[[punct]][0-9]*[[punct]][0-9]*/$ip/" ./hosts.ini
+sed -i "" "4s/[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*$/$ip/" ./hosts.ini
 
 vm="mzmaster"
 ip=$(az vm show -g mosip-sandbox-test -n $vm --query privateIps -d --out tsv)
 sed -i "" "8s/$vm.sb/$vm/" ./hosts.ini
-sed -i "" "8s/10.20.20.99/$ip/" ./hosts.ini
+sed -i "" "8s/[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*$/$ip/" ./hosts.ini
 
 vm="mzworker0"
 ip=$(az vm show -g mosip-sandbox-test -n $vm --query privateIps -d --out tsv)
 sed -i "" "10s/$vm.sb/$vm/" ./hosts.ini
-sed -i "" "10s/10.20.20.100/$ip/" ./hosts.ini
+sed -i "" "10s/[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*$/$ip/" ./hosts.ini
 
 vm="mzworker1"
 ip=$(az vm show -g mosip-sandbox-test -n $vm --query privateIps -d --out tsv)
 sed -i "" "11s/$vm.sb/$vm/" ./hosts.ini
-sed -i "" "11s/10.20.20.101/$ip/" ./hosts.ini
+sed -i "" "11s/[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*$/$ip/" ./hosts.ini
 
 vm="mzworker2"
 ip=$(az vm show -g mosip-sandbox-test -n $vm --query privateIps -d --out tsv)
 sed -i "" "12s/$vm.sb/$vm/" ./hosts.ini
-sed -i "" "12s/10.20.20.102/$ip/" ./hosts.ini
+sed -i "" "12s/[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*$/$ip/" ./hosts.ini
 
 vm="dmzmaster"
 ip=$(az vm show -g mosip-sandbox-test -n $vm --query privateIps -d --out tsv)
 sed -i "" "22s/$vm.sb/$vm/" ./hosts.ini
-sed -i "" "22s/10.20.20.199/$ip/" ./hosts.ini
+sed -i "" "22s/[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*$/$ip/" ./hosts.ini
 
 vm="dmzworker0"
 ip=$(az vm show -g mosip-sandbox-test -n $vm --query privateIps -d --out tsv)
 sed -i "" "24s/$vm.sb/$vm/" ./hosts.ini
-sed -i "" "24s/10.20.20.200/$ip/" ./hosts.ini
+sed -i "" "24s/[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*$/$ip/" ./hosts.ini
 
 git add hosts.ini
 git add ./group_vars/all.yml
