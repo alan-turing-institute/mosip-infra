fdc39e6f0215afaa242d2c834b106426da65ad46
callummole <cmole@turing.ac.uk>
2020-11-26 09:00:47 +0000

    remove to run in dir

--- deployment/sandbox-v2/automated_MOSIP_deployment.sh
+++ deployment/sandbox-v2/automated_MOSIP_deployment.sh
@@ -7,11 +7,11 @@ brew install terraform
 brew install azure-cli
 #hard-coded edits
 echo "STEP 2: CORRECTING HARDCODED EDITS"
-cd ~/
-git clone https://github.com/alan-turing-institute/mosip-infra/
-cd mosip-infra
-git checkout simple-mac-deploy-1.1.2
-cd deployment/sandbox-v2
+#cd ~/
+#git clone https://github.com/alan-turing-institute/mosip-infra/
+#cd mosip-infra
+#git checkout simple-mac-deploy-1.1.2
+#cd deployment/sandbox-v2
 chmod u+x ./turing_azure_edits.sh
 ./turing_azure_edits.sh
 # azure terraform deployment
