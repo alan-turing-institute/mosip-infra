30b30f4a77b311d6c4867359f2109f3d50841a5b
callummole <cmole@turing.ac.uk>
2020-11-26 09:12:22 +0000

    edit mosip ips

--- deployment/sandbox-v2/run_on_vm.sh
+++ deployment/sandbox-v2/run_on_vm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # script to install MOSIP on vm
-az vm run-command invoke -g mosip-sandbox-test -n console --command-id RunShellScript --scripts @install_mosip.sh
+az vm run-command invoke -g mosip-sandbox-test -n console --command-id RunShellScript --scripts @install_MOSIP_vm.sh
