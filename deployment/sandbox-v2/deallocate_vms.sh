2fa1568e4c9267673883186bd3c0e069390e7770
callummole <cmole@turing.ac.uk>
2020-11-30 13:28:04 +0000

    add progress messages to azure automation

--- deployment/sandbox-v2/deallocate_vms.sh
+++ deployment/sandbox-v2/deallocate_vms.sh
@@ -3,9 +3,16 @@
 # script to start up vms.
 az account set -s a8604fcd-7cc8-4801-a975-a4b777a179a4
 az vm deallocate --resource-group mosip-sandbox-test --name console
+echo "console deallocated 1/7"
 az vm deallocate --resource-group mosip-sandbox-test --name mzmaster
+echo "mzmaster deallocated 2/7"
 az vm deallocate --resource-group mosip-sandbox-test --name mzworker0
+echo "mzworker0 deallocated 3/7"
 az vm deallocate --resource-group mosip-sandbox-test --name mzworker1
+echo "mzworker1 deallocated 4/7"
 az vm deallocate --resource-group mosip-sandbox-test --name mzworker2
+echo "mzworker2 deallocated 5/7"
 az vm deallocate --resource-group mosip-sandbox-test --name dmzmaster
+echo "dmzmaster deallocated 6/7"
 az vm deallocate --resource-group mosip-sandbox-test --name dmzworker0
+echo "dmzworker0 deallocated 7/7"
--- deployment/sandbox-v2/stop_vms.sh
+++ deployment/sandbox-v2/stop_vms.sh
@@ -3,9 +3,16 @@
 # script to start up vms.
 az account set -s a8604fcd-7cc8-4801-a975-a4b777a179a4
 az vm stop --resource-group mosip-sandbox-test --name console
+echo "console stopped 1/7"
 az vm stop --resource-group mosip-sandbox-test --name mzmaster
+echo "mzmaster stopped 2/7"
 az vm stop --resource-group mosip-sandbox-test --name mzworker0
+echo "mzworker0 stopped 3/7"
 az vm stop --resource-group mosip-sandbox-test --name mzworker1
+echo "mzworker1 stopped 4/7"
 az vm stop --resource-group mosip-sandbox-test --name mzworker2
+echo "mzworker2 stopped 5/7"
 az vm stop --resource-group mosip-sandbox-test --name dmzmaster
+echo "dmzmaster stopped 6/7"
 az vm stop --resource-group mosip-sandbox-test --name dmzworker0
+echo "dmzworker0 stopped 7/7"
