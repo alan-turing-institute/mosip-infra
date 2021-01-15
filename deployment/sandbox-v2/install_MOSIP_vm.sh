af82339ae2834398487f95e5edf532dd3fc39d25
callummole <cmole@turing.ac.uk>
2021-01-15 09:31:24 +0000

    consolidate shell scripts

--- deployment/sandbox-v2/install_MOSIP_vm.sh
+++ deployment/sandbox-v2/install_MOSIP_vm.sh
@@ -6,8 +6,8 @@ git clone https://github.com/alan-turing-institute/mosip-infra
 cd mosip-infra
 git checkout 1.1.3
 cd deployment/sandbox-v2
+cp $HOME/mosip-infra/deployment/sandbox-v2/utils/tmux.conf $HOME/.tmux.conf
 ./keys.sh hosts.ini
-./preinstall.sh
-source ~/.bashrc
+chmod u+x ./individual_playbooks.sh
 ./individual_playbooks.sh
 #an site.yml
--- deployment/sandbox-v2/terraform/azure/console.sh
+++ deployment/sandbox-v2/terraform/azure/console.sh
@@ -6,6 +6,11 @@ echo "------------ Generating the SSH-Key on Console Machine -------------"
 
 ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ''
 
+echo " ------------- Installing Ansible on Console Machine ---------------"
+
+echo "$PW" | sudo -E -S yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+echo $PW | sudo yum install ansible -y
+
 echo "------------- Installing GIT on Console Machine ------------"
 
 echo $PW | sudo yum install -y git
@@ -15,6 +20,21 @@ echo "-------------- Disabling FirewallD -----------------"
 echo $PW | sudo systemctl stop firewalld
 echo $PW | sudo systemctl disable firewalld 
 
+echo "------------------- Injecting Aliases ----------------------------"
+
+echo "###################################################################################################" >> $HOME/.bashrc
+echo "# Aliases" >> $HOME/.bashrc
+echo "###################################################################################################" >> $HOME/.bashrc
+echo "alias an='ansible-playbook -i hosts.ini --ask-vault-pass -e @secrets.yml'" >> $HOME/.bashrc
+echo "alias av='ansible-vault'" >> $HOME/.bashrc
+echo "alias kc1='kubectl --kubeconfig $HOME/.kube/mzcluster.config'" >> $HOME/.bashrc
+echo "alias kc2='kubectl --kubeconfig $HOME/.kube/dmzcluster.config'" >> $HOME/.bashrc
+echo "alias sb='cd $HOME/mosip-infra/deployment/sandbox-v2/'" >> $HOME/.bashrc
+echo "alias helm1='helm --kubeconfig $HOME/.kube/mzcluster.config'" >> $HOME/.bashrc
+echo "alias helm2='helm --kubeconfig $HOME/.kube/dmzcluster.config'" >> $HOME/.bashrc
+echo "alias helmm='helm --kubeconfig $HOME/.kube/mzcluster.config -n monitoring'" >> $HOME/.bashrc
+echo "alias kcm='kubectl -n monitoring --kubeconfig $HOME/.kube/mzcluster.config'" >> $HOME/.bashrc
+source  ~/.bashrc
 
 echo "----------------- Adding Mosipuser to Sudoers file -------------------------"
 
@@ -23,13 +43,34 @@ ls
 echo "I am root now"
 echo 'mosipuser ALL=(ALL)  NOPASSWD: ALL' >> /etc/sudoers
 yum install tmux -y
-cp /utils/tmux.conf ~/.tmux.conf
 mkdir -p ~/.ssh
 touch ~/.ssh/authorized_keys
 echo "-------------------- Copying mosipuser key to root --------------------------"
 cat /home/mosipuser/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 
+chmod 755 /home/mosipuser # Needed for nginx access to display files
 exit
 EOF
 
+# Old file system stuff if needed
+# Partition disk to use the additional data.
+# https://docs.microsoft.com/en-us/azure/virtual-machines/linux/attach-disk-portal
+# CAUTION: the partition name is hardcoded. It may change.
+### The previous code partitioned the drive but didn't mount it.
+## see also https://blog.wacekdziewulski.ovh/2020/02/20/aws-cloudformation-mount-volume/
 
+### ON further investigation the 128 gb disk is mounted in the same way as the default disk size is in the other vms.#
+### So probably do not need to do below
+#parted /dev/sdc --script mklabel gpt mkpart xfspart xfs 0% 100%
+#mkfs.xfs /dev/sdc1
+#partprobe /dev/sdc1
+#mkdir /srv
+#mount /dev/sdc1 /srv
+#NEW_VOLUME_UUID=`lsblk /dev/sdc1 -n -o UUID`
+
+#mkfs -t xfs /dev/nvme1n1
+#mount /dev/nvme1n1 /srv
+# Make the above permanent
+#echo "/dev/sdc1 /srv                       xfs     defaults        0 0" >> /etc/fstab
+#echo "UUID=${NEW_VOLUME_UUID}     /srv     xfs    defaults  0   0" >> /etc/fstab
+         
