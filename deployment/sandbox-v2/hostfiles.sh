93b26fa53a278dc8b20f421789665a2129e352d6
callummole <cmole@turing.ac.uk>
2021-01-19 17:50:30 +0000

    change to bash script

--- deployment/sandbox-v2/hostfiles.sh
+++ deployment/sandbox-v2/hostfiles.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # script to add entry to hostfile
 rg=mosip-sandbox-test
@@ -7,6 +7,6 @@ az account set -s a8604fcd-7cc8-4801-a975-a4b777a179a4
 
 for node in mzmaster.sb mzworker0.sb mzworker1.sb mzworker2.sb dmzmaster.sb dmzworker0.sb
 do
-az vm run-command invoke -g $rg -n $node --command-id RunShellScript --scripts "sudo echo '10.20.20.10 console.sb' >> /etc/hosts"
-echo "adding console.sb to /etc/hosts on $node" 
+    az vm run-command invoke -g $rg -n $node --command-id RunShellScript --scripts "sudo echo '10.20.20.10 console.sb' >> /etc/hosts"
+    echo "adding console.sb to /etc/hosts on $node" 
 done
