7689518c71624ff0a85ba5599c1dc6f7abf89bad
callummole <cmole@turing.ac.uk>
2021-01-19 16:06:15 +0000

    start nginx and disable swapoff on reboot

--- deployment/sandbox-v2/startup_vms.sh
+++ deployment/sandbox-v2/startup_vms.sh
@@ -1,18 +1,48 @@
 #!/bin/sh
 
 # script to start up vms.
+#az vm start --ids $(az vm list -g <resourcegroupname> --query "[].id" -o tsv)
+rg=mosip-sandbox-test
+
 az account set -s a8604fcd-7cc8-4801-a975-a4b777a179a4
-az vm start --resource-group mosip-sandbox-test --name console.sb
+echo "Starting console.sb"
+az vm start --resource-group $rg --name console.sb 
 echo "console.sb started 1/7"
-az vm start --resource-group mosip-sandbox-test --name mzmaster.sb
+
+echo "starting nginx on console.sb"
+az vm run-command invoke -g $rg -n console.sb --command-id RunShellScript --scripts "sudo systemctl enable nginx && sudo systemctl start nginx"
+echo "nginx started on console.sb"
+
+echo "Starting mzmaster.sb"
+az vm start --resource-group $rg --name mzmaster.sb
 echo "mzmaster.sb started 2/7"
-az vm start --resource-group mosip-sandbox-test --name mzworker0.sb
+
+echo "disabling swapoff on mzmaster.sb"
+az vm run-command invoke -g $rg -n mzmaster.sb --command-id RunShellScript --scripts "sudo -i && swapoff -a"
+echo "disabled swapoff on mzmaster.sb"
+
+echo "Starting mzworker0.sb"
+az vm start --resource-group $rg --name mzworker0.sb
 echo "mzworker0.sb started 3/7"
-az vm start --resource-group mosip-sandbox-test --name mzworker1.sb
+
+echo "Starting mzworker1.sb"
+az vm start --resource-group $rg --name mzworker1.sb
 echo "mzworker1.sb started 4/7"
-az vm start --resource-group mosip-sandbox-test --name mzworker2.sb
+
+echo "Starting mzworker2.sb"
+az vm start --resource-group $rg --name mzworker2.sb 
 echo "mzworker2.sb started 5/7"
-az vm start --resource-group mosip-sandbox-test --name dmzmaster.sb
+
+echo "Starting dmzworker.sb"
+az vm start --resource-group $rg --name dmzmaster.sb 
 echo "dmzmaster.sb started 6/7"
-az vm start --resource-group mosip-sandbox-test --name dmzworker0.sb
+
+echo "disabling swapoff on dmzmaster.sb"
+az vm run-command invoke -g $rg -n dmzmaster.sb --command-id RunShellScript --scripts "sudo -i && swapoff -a"
+echo "disabled swapoff on dmzmaster.sb"
+
+echo "starting dmzworker0.sb"
+az vm start --resource-group $rg --name dmzworker0.sb
 echo "dmzworker0.sb started 7/7"
+
+
