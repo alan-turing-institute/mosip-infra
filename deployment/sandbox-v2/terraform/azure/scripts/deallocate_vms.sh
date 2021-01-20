c033b407a999d5a320aed28569adedfd973f7c31
callummole <cmole@turing.ac.uk>
2021-01-20 15:23:28 +0000

    typos

--- deployment/sandbox-v2/terraform/azure/scripts/deallocate_vms.sh
+++ deployment/sandbox-v2/terraform/azure/scripts/deallocate_vms.sh
@@ -4,11 +4,11 @@
 #az vm deallocate --ids $(az vm list -g <resourcegroupname> --query "[].id" -o tsv)
 az account set -s a8604fcd-7cc8-4801-a975-a4b777a179a4
 
-rg=mosip_sandbox_test
+rg=mosip-sandbox-test
 
 for node in console.sb mzmaster.sb mzworker0.sb mzworker1.sb mzworker2.sb dmzmaster.sb dmzworker0.sb
 do
     echo "deallocating $node ..."
-    az vm deallocate --resource-group $rg -name $node 
+    az vm deallocate --resource-group $rg --name $node 
     echo "$node deallocated" 
 done
