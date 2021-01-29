4e135201ac9dbc7d28bbd8715d5f7340c19e13a6
Cloud User <mosipuser@console.sb>
2021-01-29 10:06:27 +0000

    update test config and keycloak_users.csv

--- deployment/sandbox-v2/test/regclient/config.py
+++ deployment/sandbox-v2/test/regclient/config.py
@@ -1,4 +1,4 @@
-server = 'minibox.mosip.net' 
+server = 'mosipperm.uksouth.cloudapp.azure.com' 
 keycloak_admin_user = 'admin'
 keycloak_admin_pwd = 'admin'
 masterdb_path = '/home/mosipuser/mosip-infra/deployment/sandbox-v2/tmp/commons/db_scripts/mosip_master/'
--- deployment/sandbox-v2/test/regproc/config.py
+++ deployment/sandbox-v2/test/regproc/config.py
@@ -1,4 +1,4 @@
-server = 'https://minibox.mosip.net'   #  Or internal access via ingress http://mz.ingress:30080/..
+server = 'https://mosipperm.uksouth.cloudapp.azure.com'   #  Or internal access via ingress http://mz.ingress:30080/..
 ssl_verify=True  # Set to False if self signed certificate or HTTP
 user = '110127'  # Admin user for all roles to access the APIs
 password = 'Techno@123'
